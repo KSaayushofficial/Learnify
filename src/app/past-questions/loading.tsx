83751bb2949325fd5d2be9dcc0244ac4403692d7
KSaayushofficial <ksaayushofficial3@gmail.com>
2025-04-11 21:22:26 +0545

    loading

--- src/app/past-questions/loading.tsx
+++ src/app/past-questions/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-[60vh]">
-      <div className="relative w-16 h-16">
+      <div className="relative w-16 h-16">it's loading wait
         <div className="absolute top-0 left-0 w-full h-full border-4 border-primary/20 rounded-full"></div>
         <div className="absolute top-0 left-0 w-full h-full border-4 border-transparent border-t-primary rounded-full animate-spin"></div>
       </div>
